902c34d8cb05094e680702433d6f8534d84c1bea
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-10-13 00:35:57 +0530

    commit unsaved files before changing branch

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import json
-from aiohttp.web import Response
+from aiohttp.web import Response, Request
 
 
 def create_json_response(data: dict, status_code: int = 200):
@@ -26,19 +26,45 @@ def to_cordinates(location: str, default: tuple[float] = (28.653458, 77.123767))
     return co_ords
 
 
-async def get_json_data(request):
+async def get_json_data(request: Request) -> tuple:
     '''
-    extracts json data from requests
+    extracts json data from request and returns data and status code
     '''
+    status = 400
+    data = None
     try:
         if request.headers.get('Content-Type', None):
             data = await request.json()
+            status = 200
     except json.decoder.JSONDecodeError:
         data = {'err': 'Invalid JSON format'}
     else:
         data = data or {'msg': 'No JSON data found'}
 
-    return data
+    return data, status
+
+
+def to_sorted_locations_list(locations: list):
+    '''
+    converts locations list elems to floating numbers then 
+    returns sorted list based on location node weight value
+    '''
+    new_locs = []
+    # convert elems to float
+    for location in locations:
+        try:
+            new_locs.append([float(data) for data in location])
+        except ValueError:
+            pass
+
+    # sort locations based on their node weights
+    status = True
+    try:
+        new_locs = sorted(new_locs, key=lambda location: location[2])
+    except Exception:
+        status = False
+
+    return new_locs, status
 
 
 def sanitize(data: str) -> str:
--- views.py
+++ views.py
@@ -1,16 +1,16 @@
 from aiohttp.web import View, Response
-from utils import sanitize, to_cordinates
+from utils import sanitize, to_cordinates, get_json_data, create_json_response, to_sorted_locations_list
 # from rate_limiter import limiter
 
 import folium
 
 
 class Maps(View):
-    # @limiter.limit("1/min")
+    # @limiter.limit("10/sec")
     async def get(self):
         '''
         ---
-        description: This endpoint allows user to load maps and calculate optimal path between various locations
+        description: This endpoint allows user to load maps marking specified location
         tags:
         - Map
         produces:
@@ -50,3 +50,70 @@ class Maps(View):
 
         # return map in html format
         return Response(text=map._repr_html_(), content_type='text/html', status=200)
+
+    # @limiter.limit("10/sec")
+    async def post(self):
+        '''
+        ---
+        description: This endpoint allows user to load maps and calculate optimal path between various locations
+        tags:
+        - Map
+        produces:
+        - application/json
+        - text/html
+        parameters:
+        -   in: body
+            name: locations
+            description: array containing latitude, longitude and node weightage values seperated by comma to indicate location
+            schema:
+                type: object
+                properties:
+                    locations:
+                        type: array
+                        items:
+                            type: array
+                            items:
+                                type: number
+                                format: double
+                items:
+                    type: array
+            required: true
+        responses:
+            "200":
+                description: On successful operation returns html page with optimal path on map
+            "400":
+                description: JSON format is invalid or request cannot be processed
+            "405":
+                description: Method Not allowed
+        '''
+        json_data, status_code = await get_json_data(self.request)
+        if status_code != 400:
+            # extract locations and sort according to their weights
+            locations, status = to_sorted_locations_list(
+                json_data.get('locations', [[]])
+            )
+
+            resp = {
+                'msg': 'locations data required in [ [lat1, long1, node_weight1], [lat2, long2, node_weight2] ] format'}
+            status_code = 400
+            if status:
+                resp = sorted(locations, key=lambda location: location[2])
+                status_code = 200
+
+                # create map using location
+                # map = folium.Map(location=location, zoom_start=15)
+
+                # add markers
+                # folium.Marker(
+                #     location=location,
+                #     popup='location',
+                #     tooltip=str(location)
+                # ).add_to(map)
+                # folium.CircleMarker(
+                #     location=location,
+                #     radius=10
+                # ).add_to(map)
+
+                # return map in html format
+                # return Response(text=map._repr_html_(), content_type='text/html', status=200)
+        return create_json_response(resp, status_code)
