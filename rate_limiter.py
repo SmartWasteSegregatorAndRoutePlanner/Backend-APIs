c269257573c42adbf5271db80dcd61c4c298b5e5
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-10-29 19:04:51 +0530

    fix rate limiting bugs

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,14 +1,14 @@
 from aiohttplimiter import RateLimitExceeded, Limiter, default_keyfunc
-from aiohttp.web import Request
+from aiohttp.web import Request, View
 from utils import create_json_response
 
 
 
-def get_user_ip(request: Request) -> str:
+def get_user_ip(request: Request or View) -> str:
     '''
     Returns the user's IP
     '''
-    assert isinstance(request, Request)
+    assert isinstance(request, Request or View)
     
     ip = request.remote or '127.0.0.1'
     ip = ip.split(",")[0]
@@ -30,7 +30,7 @@ def rate_limit_err_handler(request: Request, exc: RateLimitExceeded):
 
 
 limiter = Limiter(
-    keyfunc=default_keyfunc,
-    exempt_ips={"127.0.0.1"},
+    keyfunc=get_user_ip,
+    # exempt_ips={"127.0.0.1"},
     error_handler=rate_limit_err_handler
 )
