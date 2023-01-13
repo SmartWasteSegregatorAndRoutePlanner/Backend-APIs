6815ab47f8fae8b0b11f5d84bc232428262213e0
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-01-13 17:58:21 +0530

    refactor: use APIView instead of ViewSet for route-planner endpoint
    create POST method for route-planner endpoint

--- route_planner/urls.py
+++ route_planner/urls.py
@@ -1,14 +1,6 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import MapViewSet
+from django.urls import path
+from .views import MapView
 
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'/map', MapViewSet, basename='map')
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', MapView.as_view(), name='route-planner'),
 ]
--- route_planner/views.py
+++ route_planner/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import HttpResponse
 from folium import Map, Marker, CircleMarker
-from rest_framework.decorators import action
-from rest_framework.viewsets import ViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .utils import sanitize, to_cordinates
-from .serializer import MapCordinateSerializer
 
-class MapViewSet(ViewSet):
-    serializer_class = MapCordinateSerializer
 
-    # /api/route-mapper/map
-    def list(self, request):
-         # get location from request url
+# /api/route-mapper/map
+class MapView(APIView):
+    queryset = []
+
+    def get(self, _):
+        # get location from request url
         location = to_cordinates(
             sanitize(self.request.GET.get('location', None))
         )
 
-        print(location)
         # create map using location
         map = Map(location=location, zoom_start=15)
 
@@ -32,3 +31,63 @@ class MapViewSet(ViewSet):
 
         # return map in html format
         return HttpResponse(map._repr_html_())
+
+    def post(self, _):
+        return Response({})
+        if status_code != 400:
+            # extract locations and sort according to their weights
+            locations, status = to_sorted_locations_list(
+                json_data.get('locations', [[]])
+            )
+
+            resp = {
+                'msg': 'locations data required in [ [lat1, long1, node_weight1], [lat2, long2, node_weight2] ] format'}
+            status_code = 400
+            if status:
+                print(locations)
+                # TODO: write logic for finding shortest distance
+
+                # define the start and end locations in latlng
+                start_latlng = (37.78497, -122.43327)
+                end_latlng = (37.78071, -122.41445)
+                mode = 'drive' # 'drive', 'bike', 'walk'# find shortest path based on distance or time
+                optimizer = 'length'# 'length','time'
+
+                graph = ox.graph_from_point(
+                    center_point=start_latlng, dist=4000, network_type=mode)
+
+                # find the nearest node to the end location
+                orig_nodes = ox.nearest_nodes(
+                    graph, X=start_latlng[1], Y=start_latlng[0])
+                dest_nodes = ox.nearest_nodes(
+                    graph, X=end_latlng[1], Y=end_latlng[0] )  # find the shortest path
+
+                shortest_route = nx.shortest_path(
+                    graph,
+                    orig_nodes,
+                    dest_nodes,
+                    weight=optimizer
+                )
+
+                # create map for shortest distance
+                shortest_route_map = ox.plot_route_folium(graph, shortest_route, tiles='openstreetmap')
+
+                # add markers
+                folium.Marker(
+                    location=start_latlng,
+                    tooltip='Start Location',
+                    popup=str(start_latlng)
+                ).add_to(shortest_route_map)
+
+                folium.Marker(
+                    location=end_latlng,
+                    tooltip='End Location',
+                    popup=str(end_latlng)
+                ).add_to(shortest_route_map)
+
+                # add markers to starting and ending location
+                return Response(text=shortest_route_map._repr_html_(), content_type='text/html', status=200)
+
+
+                status_code = 200
+
