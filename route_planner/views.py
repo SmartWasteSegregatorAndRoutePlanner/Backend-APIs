15eea9ea0597f5184a964c8bd5092fa270bb8379
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-02-04 01:44:56 +0530

    commit before implementing kruksal's algorithm

--- route_planner/views.py
+++ route_planner/views.py
@@ -92,22 +92,26 @@ class MapViewSet(ReadOnlyModelViewSet):
         center_loc = locations[0]
         center_loc = (center_loc.latitude, center_loc.longitude)
         # BUG: If you're getting some sort of key error then increase distance value
-        graph:nx.classes.multidigraph.MultiDiGraph = ox.graph_from_point(center_point=center_loc, dist=10000, network_type='drive')
+        graph: nx.classes.multidigraph.MultiDiGraph = ox.graph_from_point(
+            center_point=center_loc, dist=10000, network_type='drive')
         logging.info(f'Center Location: {locations[0]}')
-        print(graph)
 
-        # TODO: visit each node once and plot route optimally
-        # create map for shortest distance
+        # create base map
         shortest_route_map = Map(location=center_loc)
+
+        # consider minimum distance as optimal path using djikstra's algorithm
         for src_location in routes.keys():
             for dst_location in routes[src_location].keys():
-                route = routes[src_location][dst_location]['path']
-                # if route is None skip that location
+                
+                # TODO: Use Kruksal's algorithm to find minimum spanning tree and then plot routes 
+                route = None
+
+                # plot route on map if present
                 if route:
                     shortest_route_map = ox.plot_route_folium(
                         G=graph, route=route, route_map=shortest_route_map, tiles='openstreetmap')
-                    logger.info(f'Route plotted between {src_location} - {dst_location}')
-                    
+                    logger.info(
+                        f'Route plotted between {src_location} - {dst_location}')
 
         # add markers
         shortest_route_map = add_locations_to_map(
@@ -130,36 +134,36 @@ def update_routes_data(request):
 
     status_code = 201
     msg = 'Routes Updated'
-    # try:
-    # create routes from every location to another location in local memory
-    garbage_bin_locations: list[GarbageBinLocation] = set(
-        GarbageBinLocation.objects.all())
+    try:
+        # create routes from every location to another location in local memory
+        garbage_bin_locations: list[GarbageBinLocation] = set(
+            GarbageBinLocation.objects.all())
 
-    # map routes between garbage_bin_locations
-    for src_dst_pair in combinations(garbage_bin_locations, 2):
-        # extract src and dst nodes
-        src_gb_loc = src_dst_pair[0]
-        trgt_gb_loc = src_dst_pair[1]
+        # map routes between garbage_bin_locations
+        for src_dst_pair in combinations(garbage_bin_locations, 2):
+            # extract src and dst nodes
+            src_gb_loc = src_dst_pair[0]
+            trgt_gb_loc = src_dst_pair[1]
 
-        # calculate route and store it global variable
-        shortest_distance = get_shortest_distance(
-            start_loc=src_gb_loc, end_loc=trgt_gb_loc)
-        distance = ox.distance.great_circle_vec(
-            src_gb_loc.latitude, src_gb_loc.longitude, trgt_gb_loc.latitude, trgt_gb_loc.longitude)
+            # calculate route and store it global variable
+            shortest_distance = get_shortest_distance(
+                start_loc=src_gb_loc, end_loc=trgt_gb_loc)
+            distance = ox.distance.great_circle_vec(
+                src_gb_loc.latitude, src_gb_loc.longitude, trgt_gb_loc.latitude, trgt_gb_loc.longitude)
 
-        # get and update previous data
-        route = routes.get(src_gb_loc.name,{})
-        route[trgt_gb_loc.name] = {
-            'path': shortest_distance,
-            'distance': distance
-        }
-        routes[src_gb_loc.name] = route
-        logging.info(
-            f'Route Calculated Between {src_gb_loc.name} - {trgt_gb_loc.name}, distance: {distance}')
-    save_routes(routes)
+            # get and update previous data
+            route = routes.get(src_gb_loc.name, {})
+            route[trgt_gb_loc.name] = {
+                'path': shortest_distance,
+                'distance': distance
+            }
+            routes[src_gb_loc.name] = route
+            logging.info(
+                f'Route Calculated Between {src_gb_loc.name} - {trgt_gb_loc.name}, distance: {distance}')
+        save_routes(routes)
 
-    # except Exception:
-        # msg = 'Error, Check logs for more info'
-        # status_code = 500
+    except Exception:
+        msg = 'Error, Check logs for more info'
+        status_code = 500
 
     return Response({'msg': msg}, status=status_code, content_type='application/json')
