4e48f3ce27efce09bba4a0499e8a031a5353cb8e
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-04-16 17:36:39 +0530

    create label model
    register model on admin page
    
    create docker-compose file to build and test project locally

--- recognition/models.py
+++ recognition/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+labels_class_choices = [
+    'RECYCLABLE', 'recyclable',
+    'NON-RECYCLABLE', 'non-recyclable',
+    'E-WASTE', 'e-waste',
+]
+
+class GarbageLabel(models.Model):
+    label = models.CharField(max_length=255)
+    label_class = models.CharField(max_length=100, choices=labels_class_choices, default=None)
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.label} - {self.label_class}'
